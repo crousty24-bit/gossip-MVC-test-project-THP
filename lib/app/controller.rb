660a418d896dc8ec71f0370925cd5fba5aa330bc
crousty24-bit <allen.kch24@gmail.com>
2026-02-06 18:17:24 +0100

    update Gossip.new with injected hash of params created in View
    now able to store & save user new gossip in CSV file

--- lib/app/controller.rb
+++ lib/app/controller.rb
@@ -4,9 +4,10 @@ require_relative '../views/view'
 class Controller
   def initialize
     @view = View.new
+  end
   def create_gossip
     params = @view.create_gossip
-    gossip = Gossip.new("Allen", "potin potin")
+    gossip = Gossip.new(params[:author], params[:content]) #inject hash of params(from View) when new Gossip created, then saved in CSV
     gossip.save
   end
 end
